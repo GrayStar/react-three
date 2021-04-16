b629040bd65bcb71d214f0e8a270e883dd6938f5
Adam Grayson <agrayson@xmog.com>
2021-04-16 17:27:49 -0400

    npc ingame ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { OrbitControls } from '@react-three/drei';
 
 function App() {
 	return (
-		<Canvas style={{ width: 350, height: 400, backgroundColor: 'black' }}>
+		<Canvas style={{ width: '100%', height: 400, backgroundColor: 'black' }}>
 			<ambientLight />
 			<pointLight position={[10, 10, 10]} />
 
-			<Box position={[-1, 0, 0]} label="box 1" />
-			<Box position={[1, 0, 0]} label="box 2" />
+			<Box position={[-1, 0, 0]} />
+			<Box position={[1, 0, 0]} />
 
 			{/* @ts-ignore */}
 			<OrbitControls />
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,48 +1,75 @@
-import React, { useRef, useState } from 'react';
-import { MeshProps, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { GroupProps, useFrame } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import { createUseStyles } from 'react-jss';
 
 const useStyle = createUseStyles({
 	tooltip: {
+		width: 68,
+		padding: 8,
+		borderRadius: 8,
 		backgroundColor: 'white',
+		transform: 'translate(-50%, -100%)',
+	},
+	statusEffectsContainer: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	icon: {
+		width: 24,
+		height: 24,
+		marginRight: 4,
+		marginBottom: 4,
+		borderRadius: 4,
+		backgroundColor: 'gray',
+		'&:nth-child(even)': {
+			marginRight: 0,
+		},
+	},
+	bar: {
+		height: 8,
+		width: '100%',
+		borderRadius: 4,
+		backgroundColor: 'gray',
 	},
 });
 
-interface BoxProps extends MeshProps {
-	label?: string;
-}
-
-export function Box({ label, ...props }: BoxProps) {
+export function Box({ ...props }: GroupProps) {
 	const classes = useStyle();
 
-	const mesh = useRef<any>();
-	const [hovered, setHovered] = useState(false);
-	const [active, setActive] = useState(false);
+	const group = useRef<THREE.Mesh>();
 
-	useFrame((state, delta) => {
-		if (mesh.current) {
-			mesh.current.rotation.x += 0.01;
-			mesh.current.rotation.y += 0.01;
+	useFrame((_state, _delta) => {
+		if (group.current) {
+			//group.current.rotation.x += 0.01;
+			//group.current.rotation.y += 0.01;
 		}
 	});
 
 	return (
-		<mesh
-			{...props}
-			ref={mesh}
-			scale={active ? 1.5 : 1}
-			onClick={() => setActive(!active)}
-			onPointerOver={() => setHovered(true)}
-			onPointerOut={() => setHovered(false)}
-		>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-			{label && (
-				<Html scaleFactor={10}>
-					<div className={classes.tooltip}>{label}</div>
+		<group ref={group} {...props}>
+			<mesh position={[0, 0.75, 0]}>
+				<boxGeometry args={[0.5, 0.5, 0.5]} />
+				<meshStandardMaterial transparent opacity={0} />
+				<Html style={{ pointerEvents: 'none' }}>
+					<div className={classes.tooltip}>
+						<div className={classes.statusEffectsContainer}>
+							<div className={classes.icon} />
+							<div className={classes.icon} />
+							<div className={classes.icon} />
+							<div className={classes.icon} />
+							<div className={classes.icon} />
+							<div className={classes.icon} />
+						</div>
+						<div className={classes.bar} />
+						<div className={classes.bar} />
+					</div>
 				</Html>
-			)}
-		</mesh>
+			</mesh>
+			<mesh position={[0, 0, 0]}>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshStandardMaterial color="orange" />
+			</mesh>
+		</group>
 	);
 }
